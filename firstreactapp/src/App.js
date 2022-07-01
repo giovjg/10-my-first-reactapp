92d85e71bfe7facc569d1360ead2127aacb44fee
Giovanna Jaramillo-Gutierrez <gjgutierrez@protonmail.com>
2022-07-01 14:16:04 +0200

    add footer component

--- firstreactapp/src/App.js
+++ firstreactapp/src/App.js
@@ -3,6 +3,7 @@ import logo from './logo.svg';
 import HelloWorld from './Components/HelloWorld';
 import CounterExample from './Components/CounterExample';
 import Header from './Components/Header';
+import Footer from './Components/Footer';
 
 function App() {
   return (
