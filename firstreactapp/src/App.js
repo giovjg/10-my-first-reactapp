9257df8b4416cd93b73f02d3d0bad5f26664eeb4
Giovanna Jaramillo-Gutierrez <gjgutierrez@protonmail.com>
2022-07-03 14:55:19 +0200

    add styling to navigation with tailwind

--- firstreactapp/src/App.js
+++ firstreactapp/src/App.js
@@ -9,7 +9,7 @@ function App() {
   return (
     <div>
       <Header/>
-      <HelloWorld name="Giov" />
+      <HelloWorld name="Milan" />
       {/* <CounterExample/>*/}
       <Footer/>
       </div>
--- firstreactapp/src/Components/Navigation.js
+++ firstreactapp/src/Components/Navigation.js
@@ -1,13 +1,26 @@
 import React from 'react';
-import {FontAwesomeIcon, FontawesomeIcon} from '@fortawesome/react-fontawesome';
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
 import {faBars} from '@fortawesome/free-solid-svg-icons';
 
 function Navigation() {
+  const [showMenu, setShowMenu] = useState(false)
+
+  let menu
+
+  if (showMenu) {
+    menu = <div
+    className= "fixed bg-white top-0 w-4/5">
+      this is the menu
+    </div>
+  }
   return (
-    <nav className="text-xl">
-      <FontAwesomeIcon
-      icon={faBars}
-      />
+    <nav>
+        <span className="text-xl">
+          <FontAwesomeIcon
+              icon={faBars} // prop
+              onClick={() => setShowMenu(!showMenu)}
+          />
+        </span>
     </nav>
   );
 }
