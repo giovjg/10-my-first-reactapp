ebb42d97df3428f54ab5a370ef4b736f841a3770
Giovanna Jaramillo-Gutierrez <gjgutierrez@protonmail.com>
2022-07-07 14:40:57 +0200

    add react-spring

--- firstreactapp/src/Components/Navigation.js
+++ firstreactapp/src/Components/Navigation.js
@@ -1,19 +1,21 @@
-import React, {useState} from 'react';
+import React, {useState} from 'react'; // useState  from react
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
 import {faBars} from '@fortawesome/free-solid-svg-icons';
+import {useTransition, animated} from 'react-spring'
 
 // import {useTransition, animated} from 'react-spring'
 // import NavigationMenu from './NavigationMenu'
 
 function Navigation(){
   const [showMenu, setShowMenu] = useState(false);
+  // conditional rendering
   let menu
   let menuMask
     if(showMenu){
       menu =
       <div
         className="fixed bg-white top-0 w-4/5 h-full z-50 shadow">
-          This is the menu open
+          The menu is open
       </div>
       menuMask =
       <div
